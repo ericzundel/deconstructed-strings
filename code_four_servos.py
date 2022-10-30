e548ea56f7253491bf574d6a8889e96536984267
Eric Ayers <ericzundel@gmail.com>
2022-10-30 15:11:28 -0400

    Example code is working with one PIR sensor

--- code_four_servos.py
+++ code_four_servos.py
@@ -2,17 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
-"""Driving four servos on an Adafruit Feather rp2040"""
-
-# System imports
-import board
-import digitalio
-import pwmio
+"""CircuitPython Essentials Servo standard servo example"""
 import time
-
-# 3rd Party imports
+import board
+import pwmio
 from adafruit_motor import servo
-
 
 
 # create a PWMOut object on Pin SDA.
@@ -22,49 +16,43 @@ pwms.append(pwmio.PWMOut(board.D4, duty_cycle=2 ** 15, frequency=50))
 pwms.append(pwmio.PWMOut(board.D11, duty_cycle=2 ** 15, frequency=50))
 pwms.append(pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=50))
 
-# Create servo objects
-servos = []
-for i in range(0, len(pwms)):
-    servos.append(servo.Servo(pwms[i], min_pulse = 750, max_pulse = 2000))
+num_servos = len(pwms)
 
-pir = digitalio.DigitalInOut(board.RX)
-pir.direction = digitalio.Direction.INPUT
+# Create a servo object
+servos = []
+for i in range(0, num_servos):
+    servos.append(servo.Servo(pwms[i], min_pulse = 750, max_pulse = 2000))
 
 def back():
     for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        for i in range(0, len(servos)):
+        for i in range(0,num_servos):
             servos[i].angle = angle
         time.sleep(0.05)
 
 def forth():
     for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        for i in range(0, len(servos)):
+        for i in range(0,num_servos):
             servos[i].angle = angle
         time.sleep(0.05)
 
 print("Hello World!")
 
 while True:
-    if (pir.value):
-        print("PIR active!")
-        print("back")
-        back()
-        print("forth")
-        forth()
+    print("back")
+    back()
+    print("forth")
+    forth()
 
-        print("Sweep one way")
-        servos[0].angle = 0
-        servos[1].angle = 180
-        servos[2].angle = 45
-        servos[3].angle = 90
-        time.sleep(2)
+    print("Sweep one way")
+    servos[0].angle = 0
+    servos[1].angle = 180
+    servos[2].angle = 45
+    servos[3].angle = 90
+    time.sleep(2)
 
-        print("Sweep the other way")
-        servos[0].angle = 180
-        servos[1].angle = 0
-        servos[2].angle = 90
-        servos[3].angle = 45
-        time.sleep(2)
-    else:
-        print("PIR not active")
-        time.sleep(2)
+    print("Sweep the other way")
+    servos[0].angle = 180
+    servos[1].angle = 0
+    servos[2].angle = 90
+    servos[3].angle = 45
+    time.sleep(2)
